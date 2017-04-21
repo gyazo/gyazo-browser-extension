aa41c8cabce8ae692f1f9024cbc59d840b93820d
pastak <pasta0915@gmail.com>
2017-04-21 19:28:40 +0900

    install sinon-chrome

--- test/content/expander/lib/adjacentStyle.js
+++ test/content/expander/lib/adjacentStyle.js
@@ -1,9 +1,6 @@
 import test from 'ava'
-import setup from '../../../helpers/setupDom'
 
 import adjacentStyle from '../../../../src/content/expander/lib/adjacentStyle'
-
-test.before(() => setup())
 
 test.skip(t => {
   const div = document.createElement('div')
