0743b72a54cbcf97929aa378c3229b7f32ecc2f5
pastak <pasta0915@gmail.com>
2017-05-15 18:11:31 +0900

    fix storageSwitcher in Firefox

--- src/statics/option/option.js
+++ src/statics/option/option.js
@@ -1,29 +1,31 @@
-(function () {
-  'use strict'
-  let selector = document.getElementById('selector')
-  let delaySelector = document.getElementById('delay')
-  chrome.storage.sync.get({behavior: 'element', delay: 1}, function (item) {
+import storage from '../../libs/storageSwitcher'
+
+let selector = document.getElementById('selector')
+let delaySelector = document.getElementById('delay')
+storage.get({behavior: 'element', delay: 1})
+  .then((item) => {
     selector.value = item.behavior
     delaySelector.value = item.delay
   })
-  document.getElementById('element').textContent = chrome.i18n.getMessage('selectElement')
-  document.getElementById('area').textContent = chrome.i18n.getMessage('selectArea')
+document.getElementById('element').textContent = chrome.i18n.getMessage('selectElement')
+document.getElementById('area').textContent = chrome.i18n.getMessage('selectArea')
 
-  selector.addEventListener('change', function (event) {
-    chrome.storage.sync.set({behavior: event.target.value}, function () {
+selector.addEventListener('change', function (event) {
+  storage.set({behavior: event.target.value})
+    .then(() => {
       document.querySelector('.selector-save').textContent = 'Saved'
       window.setTimeout(function () {
         document.querySelector('.selector-save').textContent = ''
       }, 2500)
     })
-  })
+})
 
-  delaySelector.addEventListener('change', function (event) {
-    chrome.storage.sync.set({delay: event.target.value}, function () {
+delaySelector.addEventListener('change', function (event) {
+  storage.set({delay: event.target.value})
+    .then(() => {
       document.querySelector('.scroll-delay-save').textContent = 'Saved'
       window.setTimeout(function () {
         document.querySelector('.scroll-delay-save').textContent = ''
       }, 2500)
     })
-  })
-})()
+})
