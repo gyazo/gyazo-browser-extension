161618064bd9117018699ab6074aaa2433e05dd8
pastak <pasta0915@gmail.com>
2017-07-25 13:34:50 +0900

    :shirt:

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,3 +1,3 @@
 export const ESC_KEY_CODE = 27
 export const JACKUP_MARGIN = 30
-export const UPLOAD_LIMIT_VOLUME = 2 * 1024 * 1024 * ( 4 / 3 ) // = 2 MB
+export const UPLOAD_LIMIT_VOLUME = 2 * 1024 * 1024 * (4 / 3) // = 2 MB, is decoded Base64
