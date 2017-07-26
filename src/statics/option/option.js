9fd5f7d5c655e37f3dc8a7b4382738a7bb8e8eeb
pastak <pasta0915@gmail.com>
2017-07-26 18:59:33 +0900

    improve display of delay current setting

--- src/statics/option/option.js
+++ src/statics/option/option.js
@@ -1,9 +1,12 @@
 import storage from '../../libs/storageSwitcher'
 
+const DELAY_WORDING = ['Short', 'Normal', 'Long', 'Very Long']
+
 let selector = document.getElementById('selector')
 let fileSizeLimit = document.getElementById('fileSizeLimit')
 let fileSizeLimitCurrentSetting = document.getElementById('fileSizeLimitCurrentSetting')
 let delaySelector = document.getElementById('delay')
+let delayCurrentSetting = document.getElementById('delayCurrentSetting')
 let contextMenuSetting = document.getElementById('contextMenuSetting')
 let pasteSupportSetting = document.getElementById('pasteSupportSetting')
 
@@ -11,6 +14,7 @@ storage.get()
   .then((item) => {
     selector.value = item.behavior
     delaySelector.value = item.delay
+    delayCurrentSetting.textContent = DELAY_WORDING[item.delay]
     pasteSupportSetting.checked = item.pasteSupport
     contextMenuSetting.checked = item.contextMenu
     fileSizeLimit.value = item.fileSizeLimit
@@ -49,6 +53,7 @@ fileSizeLimit.addEventListener('change', function (event) {
 delaySelector.addEventListener('change', function (event) {
   storage.set({delay: event.target.value})
     .then(() => {
+      delayCurrentSetting.textContent = DELAY_WORDING[event.target.value]
       document.querySelector('.scroll-delay-save').textContent = 'Saved'
       window.setTimeout(function () {
         document.querySelector('.scroll-delay-save').textContent = ''
