68b29dd69a5365a97dd864a345e08cf8a5b13535
pastak <pasta0915@gmail.com>
2017-09-25 19:50:17 +0900

    Message to show users doesn't install Gyazo.app

--- src/libs/launchApp.js
+++ src/libs/launchApp.js
@@ -3,12 +3,18 @@ import thenChrome from 'then-chrome'
 const GYAZO_APP_ID = 'com.notainc.gyazo'
 const GYAZO_GIF_APP_ID = 'com.notainc.gyazo_gif'
 const launchApp = async (appId) => {
+  let appName = ''
+  if (appId === GYAZO_APP_ID) {
+    appName = 'Gyazo.app'
+  } else if (appId === GYAZO_GIF_APP_ID) {
+    appName = 'Gyazo Gif.app'
+  }
   try {
     const res = await thenChrome.runtime.sendNativeMessage(appId, {})
     return res
   } catch (e) {
     if (/exited\.$/.test(e.message)) return
-    if (confirm('Please install Gyazo.app')) chrome.tabs.create({url: 'https://gyazo.com/download'})
+    if (confirm(chrome.i18n.getMessage('installApp', appName))) chrome.tabs.create({url: 'https://gyazo.com/download'})
   }
 }
 
