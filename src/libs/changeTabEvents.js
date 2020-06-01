88c8bda83acf87735a8c9c1d434630f4cd54d77d
pastak <pasta0915@gmail.com>
2020-06-01 17:17:06 +0900

    remove tabs permission

--- src/libs/changeTabEvents.js
+++ src/libs/changeTabEvents.js
@@ -26,7 +26,7 @@ chrome.tabs.onActivated.addListener(async (activeInfo) => {
   if (tab.status === 'loading') {
     return disableButton(tab.id)
   }
-  if (tab.url.match(/^https?:/)) {
+  if (tab.url && tab.url.match(/^https?:/)) {
     enableButton(tab.id)
   } else {
     disableButton(tab.id)
@@ -37,30 +37,9 @@ chrome.tabs.onUpdated.addListener(async (tabId, changeInfo) => {
   if (changeInfo.status === 'loading') {
     disableButton(tabId)
   } else if (changeInfo.status === 'complete') {
-    const tab = await thenChrome.tabs.get(tabId)
-    if (!tab.url.match(/^https?:/)) {
-      return console.error('This Extension can run only on https? pages: ' + location.href)
-    }
-    let loaded = [false]
-    try {
-      loaded = (await thenChrome.tabs.executeScript(tabId, {
-        code: 'window.__embededGyazoContentJS'
-      }))
-    } catch (e) {}
-    if (loaded[0]) return enableButton(tabId)
-    try {
-      await thenChrome.tabs.executeScript(tabId, {
-        file: './content.js'
-      })
-    } catch (e) {
-      if (browserInfo.msedge && e.number === -2147467259) return enableButton(tabId)
-      throw e
-    }
-    await thenChrome.tabs.insertCSS(tabId, {
-      file: '/content.css'
-    })
     enableButton(tabId)
   }
+  return true
 })
 
 export {enableButton, disableButton}
