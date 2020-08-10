8ea9d08431affb6bc7cfa6e879a2758c427c9b8a
pastak <pasta0915@gmail.com>
2020-08-10 23:05:38 +0900

    use version on manifest.json from package.json

--- script/copy-manifest.js
+++ script/copy-manifest.js
@@ -1,20 +1,31 @@
-const {writeFileSync} = require('fs')
-const path = require('path')
-const type = process.argv[2]
-const extensionBaseName = type === 'teams' ? 'Gyazo Teams' : 'Gyazo'
-const extensionId = type === 'teams' ? 'gyazo-teams-extension@gyazo.com' : 'gyazo-extension@gyazo.com';
+/* eslint-env node */
+const { writeFileSync } = require('fs');
+const path = require('path');
+const type = process.argv[2];
+const extensionBaseName = type === 'teams' ? 'Gyazo Teams' : 'Gyazo';
+const extensionId =
+  type === 'teams'
+    ? 'gyazo-teams-extension@gyazo.com'
+    : 'gyazo-extension@gyazo.com';
 
-['chrome', 'firefox'].forEach(browser => {
-  const manifestBase = require(`../src/manifests/${browser}.json`)
-  const overwrite = browser === 'firefox'
-    ? {name: extensionBaseName, applications: {gecko: {id: extensionId}}}
-    : {name: extensionBaseName}
+const { version } = require('../package.json');
+
+[('chrome', 'firefox')].forEach((browser) => {
+  const manifestBase = require(`../src/manifests/${browser}.json`);
+  const overwrite =
+    browser === 'firefox'
+      ? {
+          version,
+          name: extensionBaseName,
+          applications: { gecko: { id: extensionId } },
+        }
+      : { version, name: extensionBaseName };
   const manifest = {
     ...manifestBase,
-    ...overwrite
-  }
+    ...overwrite,
+  };
   writeFileSync(
-      path.resolve(__dirname, `../dist/${type}/${browser}/manifest.json`),
-      JSON.stringify(manifest)
-    )
-})
+    path.resolve(__dirname, `../dist/${type}/${browser}/manifest.json`),
+    JSON.stringify(manifest)
+  );
+});
