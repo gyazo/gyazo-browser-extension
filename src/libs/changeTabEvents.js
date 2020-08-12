2ba5ab65f487736fc9def73a43460257e5ffc76e
pastak <pasta0915@gmail.com>
2020-08-12 15:12:57 +0900

    Revert "Merge pull request #266 from gyazo/remove-tabs"
    This reverts commit 2291bc667fb9efffeaf18382fb50ae0e2bb8425f, reversing
    changes made to b140208ab32ef14a6648931a49785394f8150b1d.

--- src/libs/changeTabEvents.js
+++ src/libs/changeTabEvents.js
@@ -25,16 +25,38 @@ chrome.tabs.onActivated.addListener(async (activeInfo) => {
   if (tab.status === 'loading') {
     return disableButton(tab.id);
   }
-  enableButton(tab.id);
+  if (tab.url.match(/^https?:/)) {
+    enableButton(tab.id);
+  } else {
+    disableButton(tab.id);
+  }
 });
 
 chrome.tabs.onUpdated.addListener(async (tabId, changeInfo) => {
   if (changeInfo.status === 'loading') {
     disableButton(tabId);
   } else if (changeInfo.status === 'complete') {
+    const tab = await thenChrome.tabs.get(tabId);
+    if (!tab.url.match(/^https?:/)) {
+      return console.error(
+        'This Extension can run only on https? pages: ' + location.href
+      );
+    }
+    let loaded = [false];
+    try {
+      loaded = await thenChrome.tabs.executeScript(tabId, {
+        code: 'window.__embededGyazoContentJS',
+      });
+    } catch (e) {}
+    if (loaded[0]) return enableButton(tabId);
+    await thenChrome.tabs.executeScript(tabId, {
+      file: './content.js',
+    });
+    await thenChrome.tabs.insertCSS(tabId, {
+      file: '/content.css',
+    });
     enableButton(tabId);
   }
-  return true;
 });
 
 export { enableButton, disableButton };
